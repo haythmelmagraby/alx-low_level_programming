07c3bf95a89593dcb38d963a38a400ebf1420073
haythmelmagraby <haythmelmagraby@gmail.com>
2023-12-15 00:41:07 -0800

    fix betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
-int clear_bit(unsigned long int *n, unsigned int index){
+/**
+ * clear_bit - clear bit at given index
+ * @n: the long int pointer
+ * @index: the index to chang the bite value
+ *
+ * Return: (1) if (SUCCESS) and (-1) if faild
+ **/
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
 	if (index >= sizeof(*n) * 8)
-                return (-1);
+		return (-1);
 
-	if(*n & 1ul << index)
-                *n ^= 1ul << index;
+	if (*n & 1ul << index)
+		*n ^= 1ul << index;
 
-        return (1);
-
+	return (1);
 }
