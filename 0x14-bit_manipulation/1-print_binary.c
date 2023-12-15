927f349c1e20f8043d1baf02d8954359cf2c6442
haythmelmagraby <haythmelmagraby@gmail.com>
2023-12-14 23:04:58 -0800

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,27 @@
 #include "main.h"
 
+/**
+ * print_binary - convert unsigned long int to binary
+ * @n: number to be converted
+ *
+ * print n after convert
+ **/
 
-void print_binary(unsigned long int n){
+void print_binary(unsigned long int n)
+{
+	int bits = sizeof(n) * 8, counter = 0;
 
-
+	while (bits > 0)
+	{
+		if (n >> --bits & 1)
+		{
+			_putchar('1');
+			counter++;
+		} else if (counter)
+		{
+			_putchar('0');
+		}
+	}
+	if (!counter)
+		_putchar('0');
 }
