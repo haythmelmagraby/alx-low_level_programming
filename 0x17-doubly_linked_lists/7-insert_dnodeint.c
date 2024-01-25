7d6fc99031ce3203e9426aa9654ee6f2c0b63e77
haythmelmagraby <haythmelmagraby@gmail.com>
2024-01-25 14:46:51 -0800

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *node = *h, *temp = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
 
-	if (!h)
+	if (!h || !temp)
 		return (NULL);
 	while (node)
 	{
