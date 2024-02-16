9188e06c055c3cd019cf4dbfe15598edfa37c551
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-16 14:23:51 -0800

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
 	hash_node_t *h_node;
-	int f = 0;
+	char f = 0;
 
 	if (!ht || !ht->array)
 		return;
@@ -17,7 +17,7 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		h_node = ht->array[i];
 
-		while (h_node)
+		while (h_node != NULL)
 		{
 			if (f == 1)
 				printf(", ");
