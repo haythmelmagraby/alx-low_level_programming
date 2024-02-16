0bf5281df894bcf2fdea3bffdf59bdfca89dd824
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-16 06:39:52 -0800

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (h_node)
 		{
 			if (f == 1)
-				pritnf(", ");
+				printf(", ");
 			f = 1;
 			printf("'%s': '%s'", h_node->key, h_node->key);
 			h_node = h_node->next;
