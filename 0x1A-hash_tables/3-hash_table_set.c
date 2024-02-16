dbedaa5ddbcfaa5d580f74070b3356af0d7b1b9f
haythmelmagraby <haythmelmagraby@gmail.com>
2024-02-16 14:28:48 -0800

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	char *str;
 
-	if (!ht || !key || !value || ht->array == 0
+	if (!ht || !key || !value || !ht->array
 			|| ht->size == 0 || strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
