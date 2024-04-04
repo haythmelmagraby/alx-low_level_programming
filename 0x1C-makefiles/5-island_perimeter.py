ae4d9c8649fa6f5d07d01dda4f10392cbf8e3589
haythmelmagraby <haythmelmagraby@gmail.com>
2024-04-04 00:44:14 -0700

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,9 +13,9 @@ def island_perimeter(grid):
         for n in range(width):
             if grid[i][n] == 1:
                 size += 1
-                if(i > 0 and grid[i - 1][n] == 1):
+                if (i > 0 and grid[i - 1][n] == 1):
                     e += 1
-                if(n > 0 and grid[i][n - 1] == 1):
+                if (n > 0 and grid[i][n - 1] == 1):
                     e += 1
 
     return size * 4 - e * 2
